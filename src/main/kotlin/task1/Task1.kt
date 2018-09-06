c40ef285819241854b233ebb4f75d7786323cbb7
dshablov <shablov.d@gmail.com>
2018-09-06 19:01:43 +0300

    правильно читаю .cvs. Доделать чтение md5

--- src/main/kotlin/task1/Task1.kt
+++ src/main/kotlin/task1/Task1.kt
@@ -2,13 +2,14 @@ package task1
 
 import org.apache.commons.codec.digest.DigestUtils
 import java.io.File
+import java.io.FileInputStream
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
 import kotlin.streams.toList
 
-const val CSV_FORMAT_NAME = ".csv"
 const val MD5_FORMAT_NAME = ".md5"
+const val CSV_FORMAT_NAME = ".csv"
 
 fun checkEquelsMd5AndCsvFiles(pathToFiles: String): Boolean {
     val files = getFiles(pathToFiles)
@@ -17,21 +18,25 @@ fun checkEquelsMd5AndCsvFiles(pathToFiles: String): Boolean {
         throw Exception("Не удалось обнаружить файлы в папке ${pathToFiles}")
     }
 
-    val contentOfFormatMD5 = getContentFromFilesByFormat(files, CSV_FORMAT_NAME)
-    println(getMD5Hex(contentOfFormatMD5))
+    println(getContentFromMd5File(files))
+
+    val fis = FileInputStream(files.get(0))
+    val md5 = DigestUtils.md5Hex(fis)
+    println(md5)
+
 
     return false
 }
 
-fun getContentFromFilesByFormat(files: List<File>, format: String): String {
+fun getContentFromMd5File(files: List<File>): String {
     var content: String? = null
     for (file in files) {
-        if (file.name.contains(format)) {
-            content = Scanner(file).useDelimiter("\\Z").next()
+        if (file.name.contains(MD5_FORMAT_NAME)) {
+            content = Scanner(file).useDelimiter("\\n").next().trim()
         }
     }
 
-    if (content != null) return content else throw Exception("Не удалось обнаружить файлы формата ${format} в папке")
+    if (content != null) return content else throw Exception("Не удалось обнаружить файлы формата ${MD5_FORMAT_NAME} в папке")
 }
 
 //TODO воткнуть проверку файла на формат
@@ -40,5 +45,3 @@ fun getFiles(pathTo: String) = Files
         .filter { !it.toFile().isDirectory }
         .map { it.toFile() }
         .toList()
-
-fun getMD5Hex(fileContext: String) = DigestUtils.md5Hex(fileContext)
